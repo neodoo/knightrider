735dfd754b89bac940519dd1b89e1f2ce4bd33b2
fsolans <francisco.solans@neodoo.es>
2017-08-22 21:10:21 +0200

    #2 Added project for testing Keycloak cliente class we will use in others projects. We have also created a main class for initialize once time keycloak configuration.

---
+++ keycloak-client/src/main/java/com/neodoo/knigthrider/Initialize.java
@@ -0,0 +1,60 @@
+package com.neodoo.knigthrider;
+
+import com.neodoo.knigthrider.authentication.ServerManagement;
+
+public class Initialize {
+
+	public static final String KEYCLOAK_SERVER = "http://localhost:8080/auth";
+	
+	public static final String KEYCLOAK_REALM = "master";
+
+	public static final String KEYCLOAK_CLIENT = "admin-cli";
+	
+	public static final String KEYCLOAK_ADMIN_USERNAME = "admin";
+	
+	public static final String KEYCLOAK_ADMIN_PASSWORD = ".admin8$";
+	
+	public static final String KEYCLOAK_KNIGHTRIDER_REALM = "knightrider_realm";
+	
+	public static final String KEYCLOAK_KNIGHTRIDER_CLIENT = "knightrider_client";
+
+	public static final String KEYCLOAK_KNIGHTRIDER_USERNAME = "demo";
+	
+	public static final String KEYCLOAK_KNIGHTRIDER_PASSWORD = ".demo8$";
+	
+	
+	private static void init(String server, String realm, String username, String password, String client) {
+		
+		// Log to server
+		ServerManagement serverManagement = new ServerManagement(server, realm, username, password, client);
+		
+		// Create realm
+		serverManagement.createRealm(KEYCLOAK_KNIGHTRIDER_REALM);
+		
+		// Create client
+		serverManagement.createClient(KEYCLOAK_KNIGHTRIDER_REALM, KEYCLOAK_KNIGHTRIDER_CLIENT);
+
+		// Create user
+		serverManagement.createUser(KEYCLOAK_KNIGHTRIDER_REALM, KEYCLOAK_KNIGHTRIDER_USERNAME, KEYCLOAK_KNIGHTRIDER_PASSWORD, null, null, null);
+		
+	}
+	
+	public static void main( String[] args ) {
+	
+		if (args.length == 1) { 
+			
+			String server = args[0];
+			String urlServer = KEYCLOAK_SERVER.replace("localhost", server);
+			 
+			init(urlServer, KEYCLOAK_REALM, KEYCLOAK_ADMIN_USERNAME, KEYCLOAK_ADMIN_PASSWORD, KEYCLOAK_CLIENT);
+		
+		} else {
+		
+			System.err.println("Use: Initialize <hostname> where <ip> is Keycloak hostname server.\n");
+			System.exit(1);
+		
+		}
+		
+	}
+
+}
---
+++ keycloak-client/src/main/java/com/neodoo/knigthrider/authentication/ServerManagement.java
@@ -0,0 +1,92 @@
+package com.neodoo.knigthrider.authentication;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.keycloak.admin.client.*;
+import org.keycloak.representations.idm.*;
+import org.keycloak.admin.client.resource.*;
+
+public class ServerManagement {
+
+	private Keycloak keycloak;
+	
+	public ServerManagement(String server, String realm, String username, String password, String client) {
+		this.keycloak = Keycloak.getInstance(server, realm, username, password, client);
+	}
+	
+	private RealmResource getRealm(String realmName) {
+		
+		RealmResource realm = keycloak.realm(realmName);
+		
+		return realm;
+		
+	}
+
+	public void createRealm(String realmName) {
+		
+		RealmRepresentation realm = new RealmRepresentation();
+		realm.setRealm(realmName);
+
+		keycloak.realms().create(realm);
+		
+	}
+
+	public void createClient(String realmName, String clientId) {
+		
+		ClientRepresentation client = new ClientRepresentation();
+		client.setClientId(clientId);
+		//client.setRootUrl("http://foo");
+		//client.setProtocol("openid-connect");
+		
+		getRealm(realmName).clients().create(client);
+	
+	}
+	
+	public void searchUser(String realmName, String username, String firstName, String lastName, String email) {
+		
+		List<UserRepresentation> users = getRealm(realmName).users().search(username, firstName, lastName, email, 0, 10);
+		
+		users.forEach(user -> System.out.println(user.getUsername()));
+		
+	}
+
+	public void createUser(String realmName, String username, String password, String firstName, String lastName, String email) {
+		
+		UserRepresentation user = new UserRepresentation();
+		user.setUsername(username);
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setEmail(email);
+		user.setRequiredActions(Collections.<String>emptyList());
+		
+		CredentialRepresentation credential = new CredentialRepresentation();
+		credential.setType(CredentialRepresentation.PASSWORD);
+		credential.setValue(password);
+		user.setCredentials(Arrays.asList(credential));
+		
+		user.setEnabled(true);
+
+		getRealm(realmName).users().create(user);
+		
+	}
+	
+	public void updateUser(String realmName, String userUuid, String firstName, String lastName) {
+	
+		UserResource userResource = getRealm(realmName).users().get(userUuid);
+		UserRepresentation user = userResource.toRepresentation();
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		userResource.update(user);
+		
+	}
+	
+	public void deleteUser(String realmName, String userUuid) {
+		
+		UserResource userResource = getRealm(realmName).users().get(userUuid);
+		userResource.remove();
+	
+	}
+	
+}
