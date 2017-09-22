fa8d78baea157511495f6105565b0314d7296fed
herny5 <alejandrohernandezpardos@gmail.com>
2017-09-22 13:33:38 +0200

    #7 Update database exported with mysqldump.

--- renting-services/database/data.sql
+++ renting-services/database/data.sql
@@ -1,78 +1,124 @@
--- -----------------------------------------------------
--- Data for table `knightrider`.`user`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `knightrider`;
-INSERT INTO `knightrider`.`user` (`email`, `password`) VALUES ('alex@something.com', 'password');
-INSERT INTO `knightrider`.`user` (`email`, `password`) VALUES ('test', 'test');
-INSERT INTO `knightrider`.`user` (`email`, `password`) VALUES ('demo', '.demo8$');
+-- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
+--
+-- Host: localhost    Database: knightrider
+-- ------------------------------------------------------
+-- Server version	5.7.19-0ubuntu0.16.04.1
 
-COMMIT;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Dumping data for table `customer`
+--
 
--- -----------------------------------------------------
--- Data for table `knightrider`.`customer`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `knightrider`;
-INSERT INTO `knightrider`.`customer` (`email`, `name`, `surname`, `birthdate`, `mobile`, `driving_license_id`, `driving_license_date`) VALUES ('alex@something.com', 'alejandro', 'hernandez', '1991-10-02', '610264916', '73014985j', '2018-10-10');
-INSERT INTO `knightrider`.`customer` (`email`, `name`, `surname`, `birthdate`, `mobile`, `driving_license_id`, `driving_license_date`) VALUES ('test', 'are', 'test', '1992-10-02', '666666666', '43543543k', '2020-11-11');
-INSERT INTO `knightrider`.`customer` (`email`, `name`, `surname`, `birthdate`, `mobile`, `driving_license_id`, `driving_license_date`) VALUES ('demo', 'javier', 'sanchez', '1992-10-02', '666666656', '43543643k', '2018-10-10');
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES ('alex@something.com','alejandro','hernandez','1991-10-02',610264916,'73014985j','2018-10-10'),('demo','javier','sanchez','1992-10-02',666666656,'43543643k','2018-10-10'),('test','are','test','1992-10-02',666666666,'43543543k','2020-11-11');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
 
-COMMIT;
+--
+-- Dumping data for table `customer_address`
+--
 
+LOCK TABLES `customer_address` WRITE;
+/*!40000 ALTER TABLE `customer_address` DISABLE KEYS */;
+INSERT INTO `customer_address` VALUES (5,'alex@something.com','condes de aragon','16','1','50009','zaragoza','zaragoza','Spain');
+/*!40000 ALTER TABLE `customer_address` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Data for table `knightrider`.`customer_address`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `knightrider`;
-INSERT INTO `knightrider`.`customer_address` (`id`, `email`, `address`, `number`, `floor`, `postal_code`, `locality`, `province`, `state`) VALUES (DEFAULT, 'alex@something.com', 'condes de aragon', '16', '1', '50009', 'zaragoza', 'zaragoza', 'Spain');
+--
+-- Dumping data for table `customer_card`
+--
 
-COMMIT;
+LOCK TABLES `customer_card` WRITE;
+/*!40000 ALTER TABLE `customer_card` DISABLE KEYS */;
+INSERT INTO `customer_card` VALUES (2,'alex@something.com','1234567898765432','alejandro hernandez pardos',0,'2020-10-10',1);
+/*!40000 ALTER TABLE `customer_card` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `rol`
+--
 
--- -----------------------------------------------------
--- Data for table `knightrider`.`customer_card`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `knightrider`;
-INSERT INTO `knightrider`.`customer_card` (`id`, `email`, `number`, `name`, `cvs`, `date_expired`, `active`) VALUES (null, 'alex@something.com', '1234567898765432', 'alejandro hernandez pardos', 000, '2020-10-10', true);
+LOCK TABLES `rol` WRITE;
+/*!40000 ALTER TABLE `rol` DISABLE KEYS */;
+INSERT INTO `rol` VALUES ('customer');
+/*!40000 ALTER TABLE `rol` ENABLE KEYS */;
+UNLOCK TABLES;
 
-COMMIT;
+--
+-- Dumping data for table `user`
+--
 
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('alex@something.com','password'),('demo','.demo8$'),('test','test');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Data for table `knightrider`.`rol`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `knightrider`;
-INSERT INTO `knightrider`.`rol` (`rol`) VALUES ('customer');
+--
+-- Dumping data for table `user_rol`
+--
 
-COMMIT;
+LOCK TABLES `user_rol` WRITE;
+/*!40000 ALTER TABLE `user_rol` DISABLE KEYS */;
+INSERT INTO `user_rol` VALUES ('alex@something.com','customer'),('demo','customer'),('test','customer');
+/*!40000 ALTER TABLE `user_rol` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `vehicle`
+--
 
--- -----------------------------------------------------
--- Data for table `knightrider`.`user_rol`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `knightrider`;
-INSERT INTO `knightrider`.`user_rol` (`email`, `rol`) VALUES ('alex@something.com', 'customer');
-INSERT INTO `knightrider`.`user_rol` (`email`, `rol`) VALUES ('test', 'customer');
-INSERT INTO `knightrider`.`user_rol` (`email`, `rol`) VALUES ('demo', 'customer');
+LOCK TABLES `vehicle` WRITE;
+/*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
+INSERT INTO `vehicle` VALUES (1,'1010DKR','S','red','online','unblocked',40,99,41.6563497,-0.876566,1),(2,'2020RRS','S','green','online','unblocked',75,310,41.662,-0.8713,0),(3,'1818GGJ','X','blue','online','unblocked',90,454,41.65021,-0.8799,1),(4,'2323CVB','3','red','online','unblocked',25,41.659,41.659,-0.899,1);
+/*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
+UNLOCK TABLES;
 
-COMMIT;
+--
+-- Dumping data for table `vehicle_blocked`
+--
 
+LOCK TABLES `vehicle_blocked` WRITE;
+/*!40000 ALTER TABLE `vehicle_blocked` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_blocked` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Data for table `knightrider`.`vehicle`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `knightrider`;
-INSERT INTO `knightrider`.`vehicle` (`id`, `vin`, `model`, `color`, `state`, `rent_state`, `battery_level`, `battery_range`, `latitude`, `longitude`, `sun_roof`) VALUES (null, '1010DKR', 'S', 'red', 'online', 'unblocked', 40, 99, 41.6563497, -0.876566, true);
-INSERT INTO `knightrider`.`vehicle` (`id`, `vin`, `model`, `color`, `state`, `rent_state`, `battery_level`, `battery_range`, `latitude`, `longitude`, `sun_roof`) VALUES (DEFAULT, '2020RRS', 'S', 'green', 'online', 'unblocked', 75, 310, 41.6620, -0.8713, false);
-INSERT INTO `knightrider`.`vehicle` (`id`, `vin`, `model`, `color`, `state`, `rent_state`, `battery_level`, `battery_range`, `latitude`, `longitude`, `sun_roof`) VALUES (DEFAULT, '1818GGJ', 'X', 'blue', 'online', 'unblocked', 90, 454, 41.65021, -0.8799, true);
-INSERT INTO `knightrider`.`vehicle` (`id`, `vin`, `model`, `color`, `state`, `rent_state`, `battery_level`, `battery_range`, `latitude`, `longitude`, `sun_roof`) VALUES (DEFAULT, '2323CVB', '3', 'red', 'online', 'unblocked', 25, 41.659, 41.659, -0.899, true);
+--
+-- Dumping data for table `vehicle_travel`
+--
 
-COMMIT;
+LOCK TABLES `vehicle_travel` WRITE;
+/*!40000 ALTER TABLE `vehicle_travel` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_travel` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping data for table `vehicle_traveling`
+--
+
+LOCK TABLES `vehicle_traveling` WRITE;
+/*!40000 ALTER TABLE `vehicle_traveling` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_traveling` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-09-22 13:30:03
