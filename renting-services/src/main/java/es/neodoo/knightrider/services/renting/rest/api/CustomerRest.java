f55af57a6297e76e98ca8d88a0c2edff964ec241
herny5 <alejandrohernandezpardos@gmail.com>
2017-09-28 11:51:12 +0200

    #9 Updated CustomerRest and VehicleRest.

--- renting-services/src/main/java/es/neodoo/knightrider/services/renting/rest/api/CustomerRest.java
+++ renting-services/src/main/java/es/neodoo/knightrider/services/renting/rest/api/CustomerRest.java
@@ -1,17 +1,172 @@
 package es.neodoo.knightrider.services.renting.rest.api;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-public interface CustomerRest {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-	Response travelingDetails(String username) throws Exception;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
-	Response profile(String username) throws Exception;
+import es.neodoo.knightrider.services.renting.exceptions.ServiceException;
+import es.neodoo.knightrider.services.renting.model.vo.VehicleTravel;
+import es.neodoo.knightrider.services.renting.model.vo.VehicleTraveling;
+import es.neodoo.knightrider.services.renting.rest.params.ListTravelsResponse;
+import es.neodoo.knightrider.services.renting.rest.params.MyProfileResponse;
+import es.neodoo.knightrider.services.renting.rest.params.TravelingDetailResponse;
+import es.neodoo.knightrider.services.renting.rest.params.UpdateBDResponse;
+import es.neodoo.knightrider.services.renting.service.CustomerService;
+import es.neodoo.knightrider.services.renting.service.CustomerServiceImpl;
 
-	Response travels(String username) throws Exception;
+public class CustomerRest {
 
-	Response register(String email, String pass, String name, String surname, String birth, int phone,
-			String driveNumber, String driveDate, String creditCardNumber, String creditCardName, int creditCardCVS,
-			String creditCardDate);
+	private static final Log log = LogFactory.getLog(CustomerRest.class);
 
+
+	CustomerService customerService = new CustomerServiceImpl();
+	TravelingDetailResponse travelingDetailResponse = new TravelingDetailResponse();
+	MyProfileResponse myProfileResponse = new MyProfileResponse();
+	UpdateBDResponse updateBDResponse = new UpdateBDResponse();
+	ListTravelsResponse listTravelsResponse = new ListTravelsResponse();
+
+	@GET
+	@Path("/traveling_details")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response travelingDetails(@QueryParam("username") String username) {
+
+		VehicleTraveling vehicleTraveling = null;		
+		TravelingDetailResponse travelingDetailResponse = null;
+		String json = null;
+
+		try {
+
+			vehicleTraveling = customerService.getTravelingDetail(username);
+			travelingDetailResponse = this.travelingDetailResponse.buildTravelingDetailResponse(vehicleTraveling);
+
+		} catch (Exception e) {
+			travelingDetailResponse = null;
+		}		
+
+		try {
+
+			json = travelingDetailResponse.toJson();
+
+		} catch (JsonProcessingException e) {
+			log.error("Eror converting Response to Json:" + e);
+		}
+
+		return Response.status(200).entity(json).build();
+
+	}
+
+	@GET
+	@Path("/profile")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response profile(@QueryParam("username")String username) {
+
+		long numTravels = 0;
+		double time = 0;
+		double cost = 0;
+		double average = 0;
+		String name = null;
+		MyProfileResponse myProfileResponse = null;		
+		String json = null;
+
+		try {
+
+			numTravels = customerService.getNumTravels(username);
+			time = customerService.getTime(username);
+			cost = customerService.getCost(username);
+			average = customerService.getAverage(username);
+			name =customerService.getName(username);
+			myProfileResponse = this.myProfileResponse.buildMyProfileResponse(numTravels, time, cost, average, name);
+
+		} catch (Exception e) {
+			myProfileResponse = null;
+			log.error("Eror geting profile:" + e);
+		}
+
+		try {
+
+			json = myProfileResponse.toJson();
+
+		} catch (JsonProcessingException e) {
+			log.error("Eror converting Response to Json:" + e);
+		}
+
+		return Response.status(200).entity(json).build();
+
+	}
+
+	@GET
+	@Path("/travels")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response travels(@QueryParam("username")String username) {
+
+		List<VehicleTravel> travels = null;	
+		ListTravelsResponse listTravelsResponse = null;
+		String json = null;
+
+		try {
+
+			travels = customerService.getTravels(username);
+			listTravelsResponse = this.listTravelsResponse.buildTravelResponse(travels);
+
+		} catch (Exception e) {
+			listTravelsResponse = null;
+			log.error("Eror geting travels:" + e);
+		}
+
+		try {
+
+			json = listTravelsResponse.toJson();
+
+		} catch (JsonProcessingException e) {
+			log.error("Eror converting Response to Json:" + e);
+		}
+
+		return Response.status(200).entity(json).build();
+
+	}
+
+	@POST
+	@Path("/register")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response register(@QueryParam("email") String email, @QueryParam("password") String pass, @QueryParam("name") String name, @QueryParam("surname") String surname, 
+			@QueryParam("birthday") String birthDate, @QueryParam("phone") int phone, @QueryParam("driveNumber") String driveNumber, @QueryParam("driveDate") String driveDate, 
+			@QueryParam("creditNumber") String creditCardNumber, @QueryParam("creditCardName") String creditCardName, @QueryParam("creditCardCVS") int creditCardCVS, @QueryParam("creditCardDate") String creditCardDate){
+
+		boolean register = false;	
+		UpdateBDResponse registerResponse = null;
+		String json = null;
+
+		try {
+
+			register = customerService.createCustomer(email, name, surname, birthDate, phone, driveNumber, driveDate, pass, creditCardNumber, creditCardName, creditCardCVS, creditCardDate);
+			registerResponse = updateBDResponse.buildUpdateBDResponse(register, "You user was created");
+
+		} catch (ServiceException e) {
+			registerResponse = updateBDResponse.buildUpdateBDResponse(false, "Error in the register. Try with other email");
+			log.error("Error in the register:" + e);
+		} catch (Exception e){
+			registerResponse = updateBDResponse.buildUpdateBDResponse(false, "Error in the register.");
+		}
+
+		try {
+
+			json = registerResponse.toJson();
+
+		} catch (JsonProcessingException e) {
+			log.error("Eror converting Response to Json:" + e);
+		}
+
+		return Response.status(200).entity(json).build();
+	}
 }
--- renting-services/src/main/java/es/neodoo/knightrider/services/renting/rest/api/VehicleRest.java
+++ renting-services/src/main/java/es/neodoo/knightrider/services/renting/rest/api/VehicleRest.java
@@ -1,23 +1,277 @@
 package es.neodoo.knightrider.services.renting.rest.api;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-public interface VehicleRest {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-	Response test();
+import com.fasterxml.jackson.core.JsonProcessingException;
 
-	Response getVehiclesUnblocked();
+import es.neodoo.knightrider.services.renting.exceptions.ServiceException;
+import es.neodoo.knightrider.services.renting.exceptions.UserHasAnotherVehicleBlocked;
+import es.neodoo.knightrider.services.renting.exceptions.VehicleIsNotAvailable;
+import es.neodoo.knightrider.services.renting.model.vo.Vehicle;
+import es.neodoo.knightrider.services.renting.rest.params.UpdateBDParamResponse;
+import es.neodoo.knightrider.services.renting.rest.params.UpdateBDResponse;
+import es.neodoo.knightrider.services.renting.rest.params.VehicleParamResponse;
+import es.neodoo.knightrider.services.renting.rest.params.VehicleResponse;
+import es.neodoo.knightrider.services.renting.service.VehicleService;
+import es.neodoo.knightrider.services.renting.service.VehicleServiceImpl;
 
-	Response blocked(String username);
+@Path("/vehicles")
+public class VehicleRest {
 
-	Response travelingVehicles(String username);
+	private static final Log log = LogFactory.getLog(VehicleRest.class);
 
-	Response block(int vehicleId, String username);
+	VehicleService vehicleService = new VehicleServiceImpl();
+	VehicleResponse vehicleResponse = new VehicleResponse();
+	UpdateBDResponse updateBDResponse = new UpdateBDResponse();
 
-	Response unblock(int vehicleId, String username);
+	@GET
+	@Path("/test")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response test() {
 
-	Response travelStart(int vehicleId, String username);
+		Response.ResponseBuilder builder = null;
 
-	Response travelFinish(int vehicleId, String username);
+		builder = Response.ok();
+
+		return builder.build();
+
+	}
+
+	@GET
+	@Path("/state/unblocked")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getVehiclesUnblocked() {
+
+		List<Vehicle> vehiclesWithStatusUnblocked = null;
+		VehicleResponse vehicleWithStatusUnblockedResponse = null;
+		String json = null;
+
+		try {
+
+			vehiclesWithStatusUnblocked  = vehicleService.getVehiclesWithStatusUnblocked();
+			vehicleWithStatusUnblockedResponse = vehicleResponse.buildVehicleResponse(vehiclesWithStatusUnblocked);
+
+		} catch (Exception e) {
+			vehicleWithStatusUnblockedResponse = null;
+			log.error("Error geting unblocked vehicles:" + e);
+		}
+
+		try {
+
+			json = vehicleWithStatusUnblockedResponse.toJson();
+
+		} catch (JsonProcessingException e) {
+			log.error("Eror converting Response to Json:" + e);
+		}
+
+		return Response.status(200).entity(json).build();
+
+	}
+
+	@GET
+	@Path("/state/blocked")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response blocked(@QueryParam("username") String username) {
+
+		Vehicle vehicleWithStatusBlocked = null;
+		VehicleResponse vehicleWithStatusBlockedResponse = null;
+		String json = null;
+
+		try {
+
+			vehicleWithStatusBlocked = vehicleService.getVehicleWithStatusBlocked(username);
+			vehicleWithStatusBlockedResponse = vehicleResponse.buildVehicleResponse(vehicleWithStatusBlocked);
+
+		} catch (Exception e) {
+			vehicleWithStatusBlockedResponse = null;
+			log.error("Error geting blocked vehicles:" + e);
+		}
+		try {
+			json = vehicleWithStatusBlockedResponse.toJson();
+		} catch (JsonProcessingException e) {
+			log.error("Eror converting Response to Json:" + e);
+		}
+		return Response.status(200).entity(json).build();
+
+	}
+
+	@GET
+	@Path("/state/traveling")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response travelingVehicles(@QueryParam("username") String username) {
+
+		Vehicle vehicleWithStatusTraveling = null;
+		VehicleResponse vehicleWithStatusTravelingResponse = null;
+		String json = null;
+
+		try {
+
+			vehicleWithStatusTraveling = vehicleService.getVehicleWithStatusTraveling(username);
+			vehicleWithStatusTravelingResponse = vehicleResponse.buildVehicleResponse(vehicleWithStatusTraveling);
+
+		} catch (Exception e) {
+			vehicleWithStatusTravelingResponse = null;
+			log.error("Error geting traveling vehicles:" + e);
+		}		
+
+		try {
+
+			json = vehicleWithStatusTravelingResponse.toJson();
+
+		} catch (JsonProcessingException e) {
+			log.error("Eror converting Response to Json:" + e);
+		}
+		return Response.status(200).entity(json).build();
+
+	}
+
+	@POST
+	@Path("/{vehicleId}/block")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response block(@PathParam("vehicleId") int vehicleId, @QueryParam("username") String username) {
+
+		Boolean blocked = false;
+		UpdateBDResponse blockVehicleResponse = null;
+
+		String json = null;
+
+		try {
+
+			blocked = vehicleService.block(username, vehicleId);
+			blockVehicleResponse = updateBDResponse.buildUpdateBDResponse(blocked,"Succesful");
+
+		} catch (UserHasAnotherVehicleBlocked | VehicleIsNotAvailable e) {
+			blockVehicleResponse = updateBDResponse.buildUpdateBDResponse(false , e.getMessage().toString());
+			log.error("Error blocking vehicle:" + e);
+		} catch (Exception e) {
+			blockVehicleResponse = updateBDResponse.buildUpdateBDResponse(false , "An error ocurred");
+			log.error("Error blocking vehicle:" + e);
+		}
+
+		try{
+
+			json = blockVehicleResponse.toJson();
+
+		} catch (JsonProcessingException e) {
+			log.error("Eror converting Response to Json:" + e);
+		}
+
+		return Response.status(200).entity(json).build();
+
+	}
+
+	@POST
+	@Path("/{vehicleId}/unblock")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response unblock(@PathParam("vehicleId") int vehicleId, @QueryParam("username") String username) {
+
+		Boolean blocked = false;
+		UpdateBDResponse unblockVehicleResponse = null;
+
+		String json = null;
+
+		try {
+
+			blocked = vehicleService.unblock(username, vehicleId);
+			unblockVehicleResponse = updateBDResponse.buildUpdateBDResponse(blocked,"Succesful");
+
+		} catch (ServiceException e) {
+			unblockVehicleResponse = updateBDResponse.buildUpdateBDResponse(false,"An error ocurred");
+			log.error("Error unblocking vehicle:" + e);
+		} catch (Exception e){
+			log.error(e);
+			log.error("Error unblocking vehicle:" + e);
+		}
+
+		try {
+
+			json = unblockVehicleResponse.toJson();
+
+		} catch (JsonProcessingException e) {
+			log.error("Eror converting Response to Json:" + e);
+		}
+
+		return Response.status(200).entity(json).build();
+
+	}
+
+	@POST
+	@Path("/{vehicleId}/travel_start")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response travelStart(@PathParam("vehicleId") int vehicleId, @QueryParam("username") String username) {
+
+		Boolean startTravel = false;
+		UpdateBDResponse startTravelResponse = null;
+
+		String json = null;
+
+		try {
+
+			startTravel = vehicleService.travelStart(username, vehicleId);
+			startTravelResponse = updateBDResponse.buildUpdateBDResponse(startTravel,"Your travel start now");
+
+		} catch (UserHasAnotherVehicleBlocked | VehicleIsNotAvailable e) {
+			startTravelResponse = updateBDResponse.buildUpdateBDResponse(false, e.getMessage().toString());
+			log.error("Error starting travel:" + e);
+		} catch (Exception e){
+			startTravelResponse = updateBDResponse.buildUpdateBDResponse(false,"An error ocurred");
+			log.error("Error starting travel:" + e);
+		}
+
+		try {
+
+			json = startTravelResponse.toJson();
+
+		} catch (JsonProcessingException e) {
+			log.error("Eror converting Response to Json:" + e);
+		}
+
+		return Response.status(200).entity(json).build();
+
+	}
+
+	@POST
+	@Path("/{vehicleId}/travel_finish")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response travelFinish(@PathParam("vehicleId") int vehicleId, @QueryParam("username") String username) {
+
+		double finishTravelCost = 0;
+		UpdateBDResponse startTravelResponse = null;
+
+		String json = null;
+
+		try {
+
+			finishTravelCost = vehicleService.travelFinish(username, vehicleId);
+			startTravelResponse = updateBDResponse.buildUpdateBDResponse(true, String.valueOf(finishTravelCost));
+
+		}  catch (Exception e){
+			startTravelResponse = updateBDResponse.buildUpdateBDResponse(false,"An error ocurred");
+			log.error("Error finishing travel:" + e);
+		}
+
+		try {
+
+			json = startTravelResponse.toJson();
+
+		} catch (JsonProcessingException e) {
+			log.error("Eror converting Response to Json:" + e);
+		}
+
+		return Response.status(200).entity(json).build();
+
+	}
 
 }
